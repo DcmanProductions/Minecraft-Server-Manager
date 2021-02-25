842d2c6dbcbc39352763fd93d6c73310d2c9e7a4
Drew Chase <dcmanproductions@gmail.com>
2021-02-24 22:05:39 -0500

    Updated Navigation and General Styling

---
+++ OlegMC.Web/wwwroot/js/Navigation.js
@@ -0,0 +1,59 @@
+﻿
+var nav = $('#nav');
+
+loadNavigationMachanics();
+LoadView();
+setNavigationToggle();
+
+function ToggleNavigation() {
+    const collapsed = nav.hasClass('navigation-collapsed');
+    setNavigationToggle(collapsed);
+}
+
+function setNavigationToggle(collapsed = false) {
+    nav.addClass(collapsed ? "navigation-expanded" : "navigation-collapsed")
+    nav.removeClass(collapsed ? "navigation-collapsed" : "navigation-expanded")
+}
+
+function LoadView(url = 'dashboard', called = null) {
+    if (isNaN(url)) {
+        url = url.toLowerCase();
+
+        if (called != null) {
+            $('#main-area').load(`/Template/${url}`)
+            Array.from(document.getElementsByClassName('menuItem')).forEach(s => { s.classList.remove('active') })
+            called.classList.add('active')
+        } else {
+            Array.from(document.getElementsByClassName('menuItem'))[0].click();
+        }
+    } else {
+        let index = parseInt(url) + 1;
+        Array.from(document.getElementsByClassName('menuItem'))[index].click();
+    }
+    setNavigationToggle();
+}
+
+
+function SetActiveMenuItem(item) {
+    if (!isNaN(item)) {
+        let index = parseInt(item) + 1;
+        Array.from(document.getElementsByClassName('menuItem')).forEach(s => { s.classList.remove('active') })
+        Array.from(document.getElementsByClassName('menuItem'))[index].classList.add('active');
+    }
+}
+
+
+function loadNavigationMachanics() {
+    var timer;
+    nav.on({
+        mouseenter: function () {
+            timer = setTimeout(() => {
+                setNavigationToggle(true);
+            }, 1500);
+        },
+        mouseleave: function () {
+            setNavigationToggle(false);
+            clearTimeout(timer);
+        }
+    });
+}
--- OlegMC.Web/wwwroot/js/main.js
+++ OlegMC.Web/wwwroot/js/main.js
@@ -1,27 +1,3 @@
-function ChangeView(calling) {
-    $('#main-area').load(`/Template/${calling.dataset.url}`)
-    var items = document.getElementsByClassName('sidebar-item');
-    for (var i = 0; i < items.length; i++) {
-        items[i].classList.remove("sidebar-active");
-    }
-    calling.classList.add("sidebar-active");
-
-
-
-}
-
-function ChangeViewWithIndex(index) {
-    var items = document.getElementsByClassName('sidebar-item');
-    for (var i = 0; i < items.length; i++) {
-        items[i].classList.remove("sidebar-active");
-    }
-    items[index].classList.add("sidebar-active");
-}
-
-
-function resizeIFrame(frame) {
-    // frame.style.width = frame.contentWindow.document.documentElement.scrollWidth+"px";
-}
 
 function ToggleRam(e, id) {
     var text = e.innerHTML.trim();
@@ -56,32 +32,94 @@ window.onclick = function (event) {
 function loadConsole(e) {
     var console = document.getElementById(e);
     console.value = "HELLO";
-    console.value = "[06Aug2020 07:00:04.858] [main/INFO] [cpw.mods.modlauncher.Launcher/MODLAUNCHER]: ModLauncher running: args [--gameDir, ., --launchTarget, fmlserver, --fml.forgeVersion, 31.2.0, --fml.mcpVersion, 20200515.085601, --fml.mcVersion, 1.15.2, --fml.forgeGroup, net.minecraftforge, nogui]<br>[06Aug2020 07:00:04.866] [main/INFO] [cpw.mods.modlauncher.Launcher/MODLAUNCHER]: ModLauncher 5.1.0+69+master.79f13f7 starting: java version 1.8.0_265 by Private Build<br>[06Aug2020 07:00:04.891] [main/DEBUG] [cpw.mods.modlauncher.LaunchServiceHandler/MODLAUNCHER]: Found launch services [minecraft,testharness,fmlclient,fmlserver]<br>[06Aug2020 07:00:04.931] [main/DEBUG] [cpw.mods.modlauncher.NameMappingServiceHandler/MODLAUNCHER]: Found naming services : []<br>[06Aug2020 07:00:04.965] [main/DEBUG] [cpw.mods.modlauncher.LaunchPluginHandler/MODLAUNCHER]: Found launch plugins: [eventbus,object_holder_definalize,runtime_enum_extender,accesstransformer,capability_inject_definalize,runtimedistcleaner]<br>[06Aug2020 07:00:04.998] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Discovering transformation services<br>[06Aug2020 07:00:05.012] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Found additional transformation services from discovery services: []<br>[06Aug2020 07:00:05.056] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Found transformer services : [fml]<br>[06Aug2020 07:00:05.058] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Transformation services loading<br>[06Aug2020 07:00:05.060] [main/DEBUG] [cpw.mods.modlauncher.TransformationServiceDecorator/MODLAUNCHER]: Loading service fml<br>[06Aug2020 07:00:05.061] [main/DEBUG] [net.minecraftforge.fml.loading.FMLServiceProvider/]: Injecting tracing printstreams for STDOUT/STDERR.<br>[06Aug2020 07:00:05.067] [main/DEBUG] [net.minecraftforge.fml.loading.LauncherVersion/CORE]: Found FMLLauncher version 31.2<br>[06Aug2020 07:00:05.067] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: FML 31.2 loading<br>[06Aug2020 07:00:05.068] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: FML found ModLauncher version : 5.1.0+69+master.79f13f7<br>[06Aug2020 07:00:05.068] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: Initializing modjar URL handler<br>[06Aug2020 07:00:05.071] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: FML found AccessTransformer version : 2.1.1+55+master.08d32b9<br>[06Aug2020 07:00:05.073] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: FML found EventBus version : 2.2.0+59+master.4f71e48<br>[06Aug2020 07:00:05.074] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: Found Runtime Dist Cleaner<br>[06Aug2020 07:00:05.081] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: FML found CoreMod version : 2.0.3+8+master.ca72757<br>[06Aug2020 07:00:05.082] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: Found ForgeSPI package implementation version 2.1.2+9+master.a8b4d92<br>[06Aug2020 07:00:05.082] [main/DEBUG] [net.minecraftforge.fml.loading.FMLLoader/CORE]: Found ForgeSPI package specification 3<br>[06Aug2020 07:00:05.512] [main/INFO] [net.minecraftforge.fml.loading.FixSSL/CORE]: Added Lets Encrypt root certificates as additional trust<br>[06Aug2020 07:00:05.520] [main/DEBUG] [cpw.mods.modlauncher.TransformationServiceDecorator/MODLAUNCHER]: Loaded service fml<br>[06Aug2020 07:00:05.551] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Configuring option handling for services<br>[06Aug2020 07:00:05.617] [main/DEBUG] [cpw.mods.modlauncher.TransformationServicesHandler/MODLAUNCHER]: Transformation services initializing<br>[06Aug2020 07:00:05.624] [main/DEBUG] [cpw.mods.modlauncher.TransformationServiceDecorator/MODLAUNCHER]: Initializing transformation service fml<br>[06Aug2020 07:00:05.625] [main/DEBUG] [net.minecraftforge.fml.loading.FMLServiceProvider/CORE]: Setting up basic FML game directories<br>[06Aug2020 07:00:05.627] [main/DEBUG] [net.minecraftforge.fml.loading.FileUtils/CORE]: Found existing GAMEDIR directory : /var/games/minecraft/servers/SMP_Server<br>[06Aug2020 07:00:05.629] [main/DEBUG] [net.minecraftforge.fml.loading.FMLPaths/CORE]: Path GAMEDIR is /var/games/minecraft/servers/SMP_Server<br>[06Aug2020 07:00:05.629] [main/DEBUG] [net.minecraftforge.fml.loading.FileUtils/CORE]: Found existing MODSDIR directory : /var/games/minecraft/servers/SMP_Server/mods<br>[06Aug2020 07:00:05.629] [main/DEBUG] [net.minecraftforge.fml.loading.FMLPaths/CORE]: Path MODSDIR is /var/games/minecraft/servers/SMP_Server/mods<br>[06Aug2020 07:00:05.629] [main/DEBUG] [net.minecraftforge.fml.loading.FileUtils/CORE]: Found existing CONFIGDIR directory : /var/games/minecraft/servers/SMP_Server/config<br>[06Aug2020 07:00:05.630] [main/DEBUG] [net.minecraftforge.fml.loading.FMLPaths/CORE]: Path CONFIGDIR is /var/games/minecraft/servers/SMP_Server/config";
 }
 
-function SubmitForm(id) {
-    //document.getElementById(id).submit();
+function SubmitForm() {
+    $('#background-tasks')[0].innerHTML = "";
     saveJavaSettings($('#max-ram-setting-value')[0].value, $('#maxramext')[0].value, $('#min-ram-setting-value')[0].value, $('#minramext')[0].value, $('#java-args-setting')[0].value, $('#mc-args-setting')[0].value, $('#startjar option:selected').text())
-
 }
 
 function SaveProperty(e) {
-    window.location.href = '/Home/SaveProperties?key=' + e.id + '&value=' + e.value;
-    //var url = window.location.href.replace("http://", "").replace("https://", "").split("/")[0];
-    //window.open(url + '/Home/SaveProperties?key=' + e.id + '&value=' + e.value);
+    $('#background-tasks')[0].innerHTML = "";
+    $('#background-tasks').load(`/Template/properties?key=${e.id}&value=${e.value}`);
+    setTimeout(() => {
+        $('#background-tasks')[0].innerHTML = "";
+    }, 100)
 }
 
 function SendConsoleCommand(e, input) {
+    $('#background-tasks')[0].innerHTML = "";
     if (e.keyCode === 13) {
         e.preventDefault();
-        location.href = "/Home/SendMessage?msg=" + input.value;
+        let message = input.value.replace(/ /gi, "%20");
+        $('#background-tasks').load(`/Home/SendMessage?msg=${message}`);
+        $('#console-view').load("/Template/Console/Text")
+        $('#console-view')[0].scrollTop = $('#console-view')[0].scrollHeight;
+        input.value = "";
+        setTimeout(() => {
+            $('#background-tasks')[0].innerHTML = "";
+        }, 1000)
     }
 }
-
+function deleteServer() {
+    $('#background-tasks')[0].innerHTML = "";
+    $('#background-tasks').load('/Template/Status/delete');
+    setTimeout(() => {
+        $('#background-tasks')[0].innerHTML = "";
+        LoadView(0)
+        $("#navigation_side_bar").load("/Template/Sidebar");
+    }, 1000)
+}
 function addServer(servername, serverport, maxplayers, levelname, difficulty, seed) {
-    $('#background-tasks').load(`/Template/AddServer?servername=${servername}&serverport=${serverport}&maxplayers=${maxplayers}&levelname=${levelname}&difficulty=${difficulty}&seed=${seed}`);
-    ChangeViewWithIndex(0)
+    $('#background-tasks').load(`/Template/add?servername=${servername}&serverport=${serverport}&maxplayers=${maxplayers}&levelname=${levelname}&difficulty=${difficulty}&seed=${seed}`);
+    setTimeout(() => {
+        $('#background-tasks')[0].innerHTML = "";
+        LoadView(0)
+    }, 1500)
 }
 function saveJavaSettings(maxram = "", maxramext = "G", minram = "", minramext = "M", javaargs = "", mcargs = "", startjar = "") {
-    $('#background-tasks').load(`/Status/SaveJavaSettings?maxram=${maxram}&maxramext=${maxramext}&minram=${minram}&minramext=${minramext}&javaargs=${javaargs}&mcargs=${mcargs}&startjar=${startjar}`);
+
+    $('#background-tasks')[0].innerHTML = "";
+    let args = `maxram=${maxram}&maxramext=${maxramext}&minram=${minram}&minramext=${minramext}`;
+    args += (startjar === "---Select Jar---" ? "" : `&startjar=${startjar}`) + (javaargs === "" ? "" : `&javaargs=${javaargs}`);
+    console.log(args);
+    $('#background-tasks').load(`/Status/SaveJavaSettings?${args}`);//`/Status/SaveJavaSettings?maxram=${maxram}&maxramext=${maxramext}&minram=${minram}&minramext=${minramext}&javaargs=${javaargs}&mcargs=${mcargs}&startjar=${startjar}`
+    setTimeout(() => {
+        LoadView(3);
+        $('#background-tasks')[0].innerHTML = "";
+    }, 100)
+}
+
+function TryParseInt(str, value) {
+    let retValue = value;
+    if (str !== null) {
+        if (str.length > 0) {
+            if (!isNaN(str)) {
+                retValue = parseInt(str);
+            }
+        }
+    }
+    return retValue;
+}
+
+function UploadJarFile() {
+    $('#background-tasks')[0].innerHTML = "";
+    let fileUpload = $("#upload-jar-file")[0];
+    let files = fileUpload.files;
+    let data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+        data.append(files[i].name, files[i]);
+    }
+    $.ajax({
+        type: "POST",
+        url: "/UploadJarFile",
+        contentType: false,
+        processData: false,
+        data: data,
+        success: message => {
+            console.log(message);
+            SubmitForm();
+        },
+        error: message => { console.error(message); }
+    });
 }
